d7bb3e7994acfee83b30efef66171ae3e80d8f9b
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2020-12-24 22:32:03 +0900

    1224/ object in lastClass by inchoriaTT
    인철쨩이 내준 마지막 과제 ㅠㅠ

--- Main.java
+++ Main.java
@@ -1,86 +1,39 @@
-package Main.dec23;
+package Main.dec24;
 
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashMap;
 import java.util.Scanner;
 
-
-
 public class Main {
-public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	String Str=sc.next();
-	
-	if (Str.equals("1")) {
-		HashMap<Integer, String> map = new HashMap<Integer, String>();
+	public static void main(String[] args) {
 
-		map.put(6, "hello");
-		map.put(7, "java");
-		map.put(8, "world");
-		//1. hash 데이터 볼떄
-		System.out.println("1 : "+map);
-		//2. hash 크기(사이즈) 볼떄
-		System.out.println("2 : "+map.size());
-		//3. hash 키로 값 가져올때
-		System.out.println("3 : "+map.get(6));
-		//4. hash 키로 값 지울때
-		System.out.print("4 : "+map.remove(6));
-		System.out.println(map.get(6));
-		//5. hash 키로 특정 데이터의 유무확인 boolean타입
-		System.out.println("5 : "+map.containsKey(7));
-		//6. hash 값으로 특정 데이터의 유무확인 boolean타입
-		System.out.println("6 : "+map.containsValue("world"));
-		//7. 데이터 전체 제거
-		map.clear();
-		System.out.println("7 : "+map);
-		//8. 데이터 유무
-		System.out.println("8 : "+map.isEmpty());	
-	}else if (Str.equals("2")) {
-		int i= 10;
-		int j= 0;
-		int r= 0;
+		Scanner sc = new Scanner(System.in);
 		
-		try {
-			r=i / j;
-		} catch (Exception e) {
-			e.printStackTrace();
-			String msg = e.getMessage();
-			System.out.println("Message is : "+msg);
-		}
+		String str= "*";
+		int count=1;
+		while(true) {
+		int A=sc.nextInt();
+		if (A%2==0) {
+			System.out.println("홀수를 입력해주세요");
+		}else if( A%2 != 0) {
 		
-		
-		
-	}else if (Str.equals("3")) {
-		ServerSocket serversocket= null;
-		Socket socket =null; 
-		
-		
-		try {
-			
-			serversocket=new ServerSocket(9000);
-			System.out.println("클라이언트 맞을준비 on");
-			socket=serversocket.accept();
-			System.out.println("클라이언트 연결 on");
-			System.out.println("socket : "+socket);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				if (socket!=null) socket.close();
-				if (serversocket!=null)serversocket.close();
-				
-			} catch (Exception e2) {
-				// TODO: handle exception
+			for (int i = 1; i <= A+1; i=i+2) {
+				for (int j =(A-i)/2; j >0 ; j--) {
+					System.out.print(" ");
+				}
+				for (int j = 0; j < i; j++) {
+					
+					System.out.print("*");
+				}System.out.println();
+				for (int j =(A-i)/2; j >i ; j++) {
+					System.out.print(" ");
+				}
+				for (int j = 0; j < i; j++) {
+					
+					System.out.print("*");
+				}System.out.println();
 			}
 		}
-
 	}
-	
-	
-
-
+}
 }
 
 
-}
