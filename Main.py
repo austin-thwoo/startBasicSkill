c09705b1da920884ae3885cd8b441a271d8885bc
austin-thwoo <70263597+austin-thwoo@users.noreply.github.com>
2021-02-25 20:52:23 +0900

    21.02.25/python basic
    파이썬 입문

---
+++ Main.py
@@ -0,0 +1,87 @@
+# a= "10"
+# b= "2874"
+# c= "1388"
+# print("0"+a+"-"+b+"-"+c)
+
+
+# print("0{0}-{1}-{2}".format(a,b,c))
+# print("내이름은 " ,end="하현수 입니다.")
+
+# #name,age,leg=input().split(" ")
+# #print("내 이름은" +name +"이고 나이는"+age +"세입니다. 케이틀린보단 "+leg)
+
+
+# # list (배열)
+# # dic ()
+# # tuple 배열(내맘대로 수정가능, 내맘대로 수정못해)
+
+# arr = []
+# arr.append("하나")
+# arr.append("둘")
+# arr.append("셋")
+# print(arr)
+# arr.pop(2)
+# print(arr)
+# # for(i : arr){
+# # sysout(i)
+# # }
+# # for(int i = 0 ; i < arr.length; i++){
+# #
+# # }
+
+# for i in arr:
+#     print("i값은 : "+str(i))
+#     for j in range(1,5):
+#         print("j값은 : " + str(j))
+
+# for k in range(1,51):
+#     print(str(k)+"번손님 반갑습니다")
+
+    
+
+#     num = int(input())
+
+# if num == 0:
+#     print("0입니다.")
+# elif num % 2 == 0:
+#     print("짝수 입니다.")
+# else:
+#     print("홀수입니다.")
+
+# for i in range(0,11):
+#     print("현재"+str(i)+"값은 : ",end="")
+#     if i == 0:
+#         print("0입니다.")
+#     elif i % 2 == 0:
+#         print("짝수 입니다.")
+#     else:
+#         print("홀수입니다.")
+
+#print("새시작")
+#x = int(input())
+#y = int(input())
+#z = int(input())
+#count =int(0)
+#or h in range(int(z)):
+#    if  count < z:
+#        x+=y
+#        count+=1
+#    else:
+#        break
+#print(x)
+
+#print("새시작")
+#array = list(map(int,input().split()))
+#for i in range(len(array)):
+#    if array[i]>11:
+#        print("500내놓으셈 ㅋ_ㅋ")
+
+val = [[1,2],[2,1],[3,3],[4,4]]
+
+array=list(5)
+
+
+print(arr)
+
+#for i in range(5):
+#    print(arr(5))
